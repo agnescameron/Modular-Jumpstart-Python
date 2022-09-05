eae7a8fd951718e592401062c86f95a3604175e6
Agnes Cameron <agnesfcameron@gmail.com>
2022-09-05 19:19:01 +0100

    added examples

---
+++ day_1/4_timing.py
@@ -0,0 +1,27 @@
+import time
+
+print('pausing')
+
+time.sleep(1)
+
+print('for')
+
+time.sleep(1)
+
+print('dramatic')
+
+time.sleep(1)
+
+print('.')
+
+time.sleep(1)
+
+print('.')
+
+time.sleep(1)
+
+print('.')
+
+time.sleep(2)
+
+print('effect!')
---
+++ day_1/5_interactivity.py
@@ -0,0 +1,14 @@
+print('what is your name')
+
+name = input('>')
+
+print('hello', name, 'my name is agnes')
+
+
+
+print('what is your favourite food')
+
+food = input('>')
+
+print('no way', name, 'my favourite food is also', food, 'though I like it best with cheese on top')
+
